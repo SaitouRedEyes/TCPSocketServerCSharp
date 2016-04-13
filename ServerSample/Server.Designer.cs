ba75624c32bb1a124edec2a6cc092a9398334a76
SaitouRedEyes <zeca.saitou@gmail.com>
2016-04-12 21:32:54 -0300

    Basic example of a TCP Socket Server in C#.
    Basic example of a TCP Socket Server in C#.

---
+++ ServerSample/Server.Designer.cs
@@ -0,0 +1,87 @@
+﻿using System.Windows.Forms;
+
+namespace ServerSample
+{
+    partial class Server
+    {
+        private TextBox show;
+        private TextBox send;
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        { 
+            this.show = new System.Windows.Forms.TextBox();
+            this.send = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // show
+            // 
+            this.show.Location = new System.Drawing.Point(8, 40);
+            this.show.Multiline = true;
+            this.show.Name = "show";
+            this.show.Size = new System.Drawing.Size(299, 238);
+            this.show.TabIndex = 1;
+            // 
+            // send
+            // 
+            this.send.Location = new System.Drawing.Point(8, 8);
+            this.send.Name = "send";
+            this.send.Size = new System.Drawing.Size(299, 20);
+            this.send.TabIndex = 2;
+            this.send.Text = "Send to Client here:";
+            this.send.KeyDown += new System.Windows.Forms.KeyEventHandler(this.send_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 297);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(226, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Escreva \"FIM\" para se desconectar do cliente";
+            // 
+            // Server
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(320, 319);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.show);
+            this.Controls.Add(this.send);
+            this.Name = "Server";
+            this.Text = "Server";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Server_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+    }
+}
+
---
+++ ServerSample/Server.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+
+namespace ServerSample
+{
+    public partial class Server : Form
+    {
+        delegate void SetTextCallback(string text);
+
+        private Thread thread;
+        private Socket connection;
+        private NetworkStream socketStream;
+        private BinaryWriter write;
+        private BinaryReader read;
+        private TcpListener listening;
+        private int connectionsNumber;
+
+        public Server()
+        {
+            InitializeComponent();
+
+            connectionsNumber = 0;
+            WaitConnections();
+        }
+
+        private void WaitConnections()
+        {
+            listening = new TcpListener(IPAddress.Any, 9000);
+            listening.Start();
+
+            thread = new Thread(new ThreadStart(RunServer));
+            thread.Start();
+        }
+
+        private void RunServer()
+        {
+            try
+            {
+                while (true)
+                {
+                    this.SetText("Aguardando conexões...");
+                    connection = listening.AcceptSocket();
+
+                    connectionsNumber++;
+
+                    socketStream = new NetworkStream(connection);
+
+                    write = new BinaryWriter(socketStream);
+                    read = new BinaryReader(socketStream);
+
+                    this.SetText(connectionsNumber + " Conexões Recebidas!");
+                    write.Write("Conexão Efetuada!");
+
+                    send.ReadOnly = false;
+
+                    string resp = "";
+
+                    do
+                    {
+                        try
+                        {
+                            resp = read.ReadString();
+                            this.SetText(resp);
+                        }
+                        catch (Exception)
+                        {
+                            break;
+                        }
+                    } while (connection.Connected);
+
+                    this.SetText("Conexão Finalizada! \r\n");
+
+                    CloseConnection();
+                }
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.ToString());
+            }
+        }
+
+        private void SetText(string text)
+        {
+            // InvokeRequired required compares the thread ID of the
+            // calling thread to the thread ID of the creating thread.
+            // If these threads are different, it returns true.
+            if (this.show.InvokeRequired)
+            {
+                //Chamando o componente de interface ajustando o problema das thread diferentes  
+                SetTextCallback d = new SetTextCallback(SetText);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                //Quando estão ns mesma thread, o acesso é permitido.
+                this.show.Text += text + "\r\n";
+            }
+        }
+
+        private void Server_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (connection != null)
+            {
+                CloseConnection();
+            }
+
+            System.Environment.Exit(System.Environment.ExitCode);
+        }
+
+        private void send_KeyDown(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                if (e.KeyCode == Keys.Enter && connection != null)
+                {
+                    write.Write(send.Text);
+
+                    if (send.Text.Equals("FIM")) CloseConnection();
+
+                    send.Clear();
+                }
+            }
+            catch (SocketException)
+            {
+                this.SetText("Atenção! Erro...");
+            }
+        }
+
+        private void CloseConnection()
+        {
+            write.Close();
+            read.Close();
+            socketStream.Close();
+            connection.Close();
+        }
+    }
+}
